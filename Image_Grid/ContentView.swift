b26754a7457e31e4829f59a7a16e5dd0c1bf32bc
Bidisha Biswas <bdsh.14@gmail.com>
2025-09-15 22:42:21 -0600

    Use SF symbols for inital grids

--- Image_Grid/ContentView.swift
+++ Image_Grid/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+	var gridSize: Int
+	var tileSize: Double
+	var columns: [GridItem]
+
+	@State private var images: [Image?]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		LazyVGrid(columns: columns) {
+			ForEach(0..<images.count, id: \.self) { index in
+				TileView(tileSize: tileSize, offset: .zero, image: images[index])
+			}
+		}
     }
+}
+
+extension ContentView {
+	init(gridSize: Int = 3) {
+		self.gridSize = gridSize
+		self.tileSize = 350 / Double(gridSize)
+		self.columns = Array(repeating: GridItem(.fixed(tileSize), spacing: 2), count: gridSize)
+
+		let startImages = (0..<gridSize * gridSize).dropLast().map { Image(systemName: "\($0).circle") } + [nil]
+		_images = State(initialValue: startImages)
+	}
 }
 
 #Preview {
