4719615378ffebe0ae4d5f6b9b99d56649b8b4cd
Bidisha Biswas <bdsh.14@gmail.com>
2025-09-15 22:08:00 -0600

    Initial Commit

---
+++ Image_Grid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image_Grid
+//
+//  Created by Bidisha Biswas on 9/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image_Grid/Image_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_GridApp.swift
+//  Image_Grid
+//
+//  Created by Bidisha Biswas on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Image_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
